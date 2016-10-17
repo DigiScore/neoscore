86565ac37dbfbabbeb2a6953868e648e742b0f00
Andrew Yoon <andrewyoon2@gmail.com>
2016-10-17 18:18:37 -0400

    Clean up brown.py

--- brown/core/brown.py
+++ brown/core/brown.py
@@ -1,5 +1,4 @@
 from brown.interface.impl.qt import app_interface_qt
-from brown.core import line
 
 # Fetch and initialize app interface
 _app_interface_class = app_interface_qt.AppInterfaceQt
@@ -10,15 +9,6 @@ def setup(doctype='plane'):
     global _app_interface
     _app_interface = _app_interface_class()
     _app_interface.create_document(doctype)
-
-def draw_line(x1, y1, x2, y2):
-    # Create a line, draw it, and return the brown Line object
-    new_line = line.Line(x1, y1, x2, y2)
-    new_line.draw()
-    return new_line
-
-def draw_circle(x, y, radius):
-    _app_interface.draw_circle(x, y, radius)
 
 def show():
     _app_interface.show()
