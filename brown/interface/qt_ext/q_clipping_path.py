04bc5db164359a815e9a49b63ff00470db03b2e5
Andrew Yoon <andrewyoon2@gmail.com>
2016-12-31 17:15:19 -0500

    Rework QClippingPath clip logic

--- brown/interface/qt_ext/q_clipping_path.py
+++ brown/interface/qt_ext/q_clipping_path.py
@@ -28,14 +28,41 @@ class QClippingPath(QtWidgets.QGraphicsPathItem):
         """Paint, clipping by `self._clip_area`.
 
         This is an overload of `QGraphicsPathItem.paint()`"""
-        bounding_rect = self.path().boundingRect()
-        adjusted_start_x = (bounding_rect.x() if self.clip_start_x is None
-                            else float(self.clip_start_x))
-        adjusted_width = (bounding_rect.width() if self.clip_width is None
-                          else float(self.clip_width))
-        self._clip_area = QtGui.QPainterPath()
-        self._clip_area.addRect(
-            QtCore.QRectF(adjusted_start_x, bounding_rect.y(),
-                          adjusted_width, bounding_rect.height()))
-        painter.setClipPath(self._clip_area)
+        clip_area = self._create_clipping_area(
+            self.path(), self.clip_start_x, self.clip_width)
+        painter.setClipPath(clip_area)
         super().paint(painter, *args, **kwargs)
+
+    ######## PRIVATE METHODS ########
+
+    def _create_clipping_area(self, painter_path, start_x, clip_width):
+        """Create a QPainterPath indicating the area of the path to draw
+
+        Args:
+            painter_path (QPainterPath): The reference path.
+                This is used to determine the path bounding box.
+            start_x (Unit or None): The local starting position for the
+                path clipping region.
+                Use `None` to render from the start
+            width (Unit or None): The width of the path clipping region.
+                Use `None` to render to the end
+
+        Returns: QPainterPath
+        """
+        bounding_rect = painter_path.boundingRect()
+        if start_x is None:
+            start_x = bounding_rect.x()
+        else:
+            # If start_x was set behind the bounding_rect start,
+            # go with bounding_rect start
+            start_x = max(float(start_x), bounding_rect.x())
+        max_clip_width = bounding_rect.width() - start_x
+        if clip_width is None:
+            clip_width = max_clip_width
+        elif clip_width > max_clip_width:
+            clip_width = max_clip_width
+        clipping_area = QtGui.QPainterPath()
+        clipping_area.addRect(
+            QtCore.QRectF(start_x, bounding_rect.y(),
+                          clip_width, bounding_rect.height()))
+        return clipping_area
---
+++ tests/test_interface/test_qt_ext/test_q_clipping_path.py
@@ -0,0 +1,58 @@
+import unittest
+
+from PyQt5 import QtGui
+
+from brown.core import brown
+from brown.interface.qt_ext.q_clipping_path import QClippingPath
+
+
+
+class TestQClippingPath(unittest.TestCase):
+
+    def setUp(self):
+        brown.setup()
+
+    def test__create_clipping_area_covering_full_path(self):
+        painter_path = QtGui.QPainterPath()
+        painter_path.lineTo(100, 200)
+        path_item = QClippingPath(painter_path, 0, 500)
+        clip_area = path_item._create_clipping_area(painter_path, None, None)
+        expected_rect = painter_path.boundingRect()
+        result_rect = clip_area.boundingRect()
+        assert(result_rect.x() == expected_rect.x())
+        assert(result_rect.y() == expected_rect.y())
+        assert(result_rect.width() == expected_rect.width())
+        assert(result_rect.height() == expected_rect.height())
+
+    def test__create_clipping_area_starting_in_path(self):
+        painter_path = QtGui.QPainterPath()
+        painter_path.lineTo(100, 200)
+        path_item = QClippingPath(painter_path, 0, 500)
+        clip_area = path_item._create_clipping_area(painter_path, 50, None)
+        result_rect = clip_area.boundingRect()
+        assert(result_rect.x() == 50)
+        assert(result_rect.y() == painter_path.boundingRect().y())
+        assert(result_rect.width() == 50)
+        assert(result_rect.height() == painter_path.boundingRect().height())
+
+    def test__create_clipping_area_ending_in_path(self):
+        painter_path = QtGui.QPainterPath()
+        painter_path.lineTo(100, 200)
+        path_item = QClippingPath(painter_path, 0, 500)
+        clip_area = path_item._create_clipping_area(painter_path, None, 50)
+        result_rect = clip_area.boundingRect()
+        assert(result_rect.x() == 0)
+        assert(result_rect.y() == painter_path.boundingRect().y())
+        assert(result_rect.width() == 50)
+        assert(result_rect.height() == painter_path.boundingRect().height())
+
+    def test__create_clipping_area_starting_and_ending_in_path(self):
+        painter_path = QtGui.QPainterPath()
+        painter_path.lineTo(100, 200)
+        path_item = QClippingPath(painter_path, 0, 500)
+        clip_area = path_item._create_clipping_area(painter_path, 25, 30)
+        result_rect = clip_area.boundingRect()
+        assert(result_rect.x() == 25)
+        assert(result_rect.y() == painter_path.boundingRect().y())
+        assert(result_rect.width() == 30)
+        assert(result_rect.height() == painter_path.boundingRect().height())
