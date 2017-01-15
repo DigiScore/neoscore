69c0907d24dfa0a6fe796260b2ba6e52c4260aa4
Andrew Yoon <andrewyoon2@gmail.com>
2017-01-15 16:23:40 -0500

    Add basic Measure container

---
+++ brown/models/measure.py
@@ -0,0 +1,23 @@
+from brown.models.container import Container
+from brown.primitives.time_signature import TimeSignature
+
+
+class Measure(Container):
+    """A metered time measure."""
+
+    def __init__(self, duration, *args, **kwargs):
+        self._duration = duration
+        super().__init__(*args, **kwargs)
+
+    @property
+    def duration(self):
+        """Duration: The total length of the Measure"""
+        return self._duration
+
+    @property
+    def has_time_signature_change(self):
+        """Whether or not a time signature change exists in this measure.
+
+        TODO: Optimize
+        """
+        return any(isinstance(item, TimeSignature) for item in self)
