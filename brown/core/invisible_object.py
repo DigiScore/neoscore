9653d539b41123d80e66cc2cec89955681f68331
Andrew Yoon <andrewyoon2@gmail.com>
2017-03-03 00:09:20 -0500

    Remove InvisibleObjectInterface
    Seeing as it's, you know, invisible. More seriously,
    it originally existed in order to leverage Qt's built-in
    GraphicsItem parentage system, but since brown moved away
    from using it, this class is no longer needed.
    
    This makes InvisibleObject._render_complete() a no-op.

--- brown/core/invisible_object.py
+++ brown/core/invisible_object.py
@@ -1,11 +1,12 @@
-from brown.interface.invisible_object_interface import InvisibleObjectInterface
 from brown.core.graphic_object import GraphicObject
 from brown.utils.units import Unit
 
 
 class InvisibleObject(GraphicObject):
 
-    _interface_class = InvisibleObjectInterface
+    """A non-renderable object."""
+
+    _interface_class = None
 
     def __init__(self, pos, parent=None):
         """
@@ -17,6 +18,4 @@ class InvisibleObject(GraphicObject):
         super().__init__(pos, Unit(0), None, None, parent)
 
     def _render_complete(self, pos):
-        interface = self._interface_class(pos)
-        interface.render()
-        self.interfaces.add(interface)
+        pass
