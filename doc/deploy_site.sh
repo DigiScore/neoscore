ef5089c4e336fea35b3bed7755955653be88bc00
Andrew Yoon <andrewyoon2@gmail.com>
2017-04-15 18:07:38 -0400

    Fix encrypted key name in deploy script

--- doc/deploy_site.sh
+++ doc/deploy_site.sh
@@ -18,12 +18,12 @@ fi
 echo "Beginning site deploy."
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-echo "Decrpyting site ssh key"
+echo "Decrypting site ssh key"
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in doc/deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in doc/site_deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
