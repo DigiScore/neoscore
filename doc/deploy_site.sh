15045fbaf3ac50d6443a507c30570d432e046dfc
Andrew Yoon <andrewyoon2@gmail.com>
2017-04-15 18:18:52 -0400

    Rotate encrypted deploy key

--- doc/deploy_site.sh
+++ doc/deploy_site.sh
@@ -24,6 +24,7 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in doc/site_deploy_key.enc -out deploy_key -d
+echo "Adding decrypted ssh key"
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
