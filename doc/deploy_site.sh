6b05c31e21a6c774a4b2116af7cc3b3d735a6fc4
Andrew Yoon <andrewyoon2@gmail.com>
2017-04-15 20:56:24 -0400

    Try again with git add ....

--- doc/deploy_site.sh
+++ doc/deploy_site.sh
@@ -53,7 +53,7 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 git add -N --all .
-if [ -z `git diff --exit-code` ]; then
+if [ -z `git diff HEAD --exit-code` ]; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
