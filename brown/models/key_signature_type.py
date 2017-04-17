1e1a182e9db7c80d5ef58d2a016078f00479deb4
Andrew Yoon <andrewyoon2@gmail.com>
2017-04-16 22:18:40 -0400

    VirtualKeySignature -> KeySignatureType
    and add data for the whole circle of fifths

---
+++ brown/models/key_signature_type.py
@@ -0,0 +1,200 @@
+from enum import Enum
+
+from brown.models.accidental_type import AccidentalType
+
+
+flat = AccidentalType.flat
+sharp = AccidentalType.sharp
+
+
+class KeySignatureType(Enum):
+    """Common key signatures.
+
+    Key signatures are specified as `dict`s between the 7 pitch letter names
+    and either an `AccidentalType` or `None`. They are enumerated here in
+    the form `[pitch letter][f|s]_[major|minor]`. For example:
+
+    * c major is `KeySignatureType.c_major`
+    * f-sharp minor is `KeySignatureType.fs_minor`
+    * d-flat major is `KeySignature.df_major`
+
+    This enum defines the entire circle of fifths - from 0 flats to 7,
+    and 0 sharps to 7.
+    """
+
+    c_major = {
+        'a': None,
+        'b': None,
+        'c': None,
+        'd': None,
+        'e': None,
+        'f': None,
+        'g': None,
+    }
+
+    a_minor = c_major.value
+
+    df_major = {
+        'a': flat,
+        'b': flat,
+        'c': None,
+        'd': flat,
+        'e': flat,
+        'f': None,
+        'g': flat,
+    }
+
+    bf_minor = df_major.value
+
+    # Ha
+    cs_major = {
+        'a': sharp,
+        'b': sharp,
+        'c': sharp,
+        'd': sharp,
+        'e': sharp,
+        'f': sharp,
+        'g': sharp,
+    }
+
+    d_major = {
+        'a': None,
+        'b': None,
+        'c': sharp,
+        'd': None,
+        'e': None,
+        'f': sharp,
+        'g': None,
+    }
+
+    b_minor = d_major.value
+
+    ef_major = {
+        'a': flat,
+        'b': flat,
+        'c': None,
+        'd': None,
+        'e': flat,
+        'f': None,
+        'g': None,
+    }
+
+    bf_minor = ef_major.value
+
+    e_major = {
+        'a': None,
+        'b': None,
+        'c': sharp,
+        'd': sharp,
+        'e': None,
+        'f': sharp,
+        'g': sharp,
+    }
+
+    cs_minor = e_major.value
+
+    f_major = {
+        'a': None,
+        'b': flat,
+        'c': None,
+        'd': sharp,
+        'e': None,
+        'f': None,
+        'g': None,
+    }
+
+    d_minor = f_major.value
+
+    gf_major = {
+        'a': flat,
+        'b': flat,
+        'c': flat,
+        'd': flat,
+        'e': flat,
+        'f': None,
+        'g': flat,
+    }
+
+    ef_minor = gf_major.value
+
+    fs_major = {
+        'a': sharp,
+        'b': None,
+        'c': sharp,
+        'd': sharp,
+        'e': sharp,
+        'f': sharp,
+        'g': sharp,
+    }
+
+    ds_minor = fs_major.value
+
+    g_major = {
+        'a': None,
+        'b': None,
+        'c': None,
+        'd': None,
+        'e': None,
+        'f': sharp,
+        'g': None,
+    }
+
+    e_minor = g_major.value
+
+    af_major = {
+        'a': flat,
+        'b': flat,
+        'c': None,
+        'd': flat,
+        'e': flat,
+        'f': None,
+        'g': None,
+    }
+
+    f_minor = af_major.value
+
+    a_major = {
+        'a': None,
+        'b': None,
+        'c': sharp,
+        'd': None,
+        'e': None,
+        'f': sharp,
+        'g': sharp,
+    }
+
+    fs_minor = a_major.value
+
+    bf_major = {
+        'a': None,
+        'b': flat,
+        'c': None,
+        'd': None,
+        'e': flat,
+        'f': None,
+        'g': None,
+    }
+
+    g_minor = bf_major.value
+
+    b_major = {
+        'a': sharp,
+        'b': None,
+        'c': sharp,
+        'd': sharp,
+        'e': None,
+        'f': sharp,
+        'g': sharp,
+    }
+
+    gs_minor = b_major.value
+
+    cf_major = {
+        'a': flat,
+        'b': flat,
+        'c': flat,
+        'd': flat,
+        'e': flat,
+        'f': flat,
+        'g': flat,
+    }
