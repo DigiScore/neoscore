41511c4034dd050792382a16bdaf4471b260a337
Andrew Yoon <andrewyoon2@gmail.com>
2017-07-23 15:07:58 -0400

    begin gustav

---
+++ brown/managed/gustav/README.md
@@ -0,0 +1,6 @@
+# brown.managed.gustav
+
+## Traditional Western Music Notation
+
+early work-in-progress, nothing to see here...
+
---
+++ brown/managed/gustav/gustav.py
@@ -0,0 +1,31 @@
+from brown.core import brown
+
+
+"""The main module and interface for the gustav notation context."""
+
+
+instruments = []
+"""list[Instrument]: The instruments in the score.
+
+The `Instrument`s should be given in descending order.
+For example, a string quartet might be listed as:
+
+`[Violin_I, Violin_II, Viola, Cello]`
+"""
+
+
+def setup(self, instr, paper=None):
+    """Set up the `gustav` context.
+
+    In `gustav` scores, this should be used in place of `brown.setup()`.
+
+    Args:
+        instr (list[Instrument]): The instruments in the score
+        initial_paper (Paper): The paper to use in the document.
+            If `None`, this defaults to `config.DEFAULT_PAPER_TYPE`
+
+    Returns: None
+    """
+    global instruments
+    brown.setup(paper)
+    instruments = instruments
