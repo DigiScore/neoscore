fbb1e069e3ad7e10c1f3dc689fc6f89c68c4716b
Andrew Yoon <andrew@nothing-to-say.org>
2022-02-21 18:12:46 -0500

    Add hashing methods for QPainterPath and QTransform
    These were originally added for an attempted path pixmap caching
    scheme, but that approach didn't pan out. While currently unused,
    these generic hashing methods may well become useful later on, so I'm
    pulling them into the main branch for now.

---
+++ brown/interface/qt/hashing.py
@@ -0,0 +1,34 @@
+from PyQt5.QtGui import QPainterPath, QTransform
+
+
+def hash_transformed_path(path: QPainterPath, transform: QTransform) -> int:
+    path_hash = hash_path(path)
+    transform_hash = hash_transform(transform)
+    return path_hash ^ transform_hash
+
+
+# For some reason QTransform's qHash method seems to be ommited from PyQt5, so
+# we implement our own based on the fields used in Qt's implementation here:
+# github.com/qt/qtbase/blob/e05e3c776/src/gui/painting/qtransform.cpp#L778-L791
+def hash_transform(t: QTransform) -> int:
+    return hash(
+        (
+            t.m11(),
+            t.m12(),
+            t.m21(),
+            t.m22(),
+            t.dx(),
+            t.dy(),
+            t.m13(),
+            t.m23(),
+            t.m33(),
+        )
+    )
+
+
+def hash_path(path: QPainterPath) -> int:
+    h = 35891237 ^ path.fillRule()
+    for i in range(path.elementCount()):
+        el = path.elementAt(i)
+        h ^= hash((el.type, el.x, el.y))
+    return h
---
+++ tests/test_interface/test_qt/test_hashing.py
@@ -0,0 +1,54 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainterPath, QTransform
+
+from brown.interface.qt import hashing
+
+
+def test_hash_path():
+    path_1 = QPainterPath()
+    path_1.lineTo(100, 200)
+    hash_1 = hashing.hash_path(path_1)
+    # Identical paths hash identically
+    path_2 = QPainterPath()
+    path_2.lineTo(100, 200)
+    hash_2 = hashing.hash_path(path_2)
+    assert hash_1 == hash_2
+    # Paths with different fill rules hash differently
+    path_3 = QPainterPath()
+    path_3.setFillRule(Qt.FillRule.WindingFill)
+    path_3.lineTo(100, 200)
+    hash_3 = hashing.hash_path(path_3)
+    assert hash_1 != hash_3
+    # Paths with different elements hash differently
+    path_4 = QPainterPath()
+    path_4.lineTo(100, 200)
+    path_4.lineTo(123, 123)
+    hash_4 = hashing.hash_path(path_4)
+    assert hash_4 != hash_1
+
+
+def test_hash_transform():
+    transform_1 = QTransform()
+    hash_1 = hashing.hash_transform(transform_1)
+    assert hash_1 == hashing.hash_transform(QTransform())
+    transform_2 = QTransform()
+    transform_2.scale(0.1, 0.1)
+    hash_2 = hashing.hash_transform(transform_2)
+    transform_3 = QTransform()
+    transform_3.rotate(20)
+    hash_3 = hashing.hash_transform(transform_3)
+    transform_4 = QTransform()
+    transform_4.translate(1, 2)
+    hash_4 = hashing.hash_transform(transform_4)
+    assert len(set([hash_1, hash_2, hash_3, hash_4])) == 4
+
+
+def test_hash_transformed_path():
+    path = QPainterPath()
+    path.lineTo(100, 200)
+    transform_1 = QTransform()
+    hash_1 = hashing.hash_transformed_path(path, transform_1)
+    transform_2 = QTransform()
+    transform_2.scale(0.1, 0.1)
+    hash_2 = hashing.hash_transformed_path(path, transform_2)
+    assert hash_1 != hash_2
