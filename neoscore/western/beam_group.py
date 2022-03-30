bed62b7458322e27b370bcdc3656d4f0545cd56d
Andrew Yoon <andrew@nothing-to-say.org>
2022-03-29 23:25:49 -0400

    Work out conversion of beam states to actual beam path specs
    In theory this should complete the beam layout algorithm. Next steps
    are integrating it into Beam and then with chordrest, stem, etc

--- neoscore/western/beam_group.py
+++ neoscore/western/beam_group.py
@@ -1,26 +1,22 @@
 from dataclasses import dataclass
-from enum import Enum, auto
 from typing import NamedTuple, Optional, cast
 
+from neoscore.models.directions import HorizontalDirection
+
 # wip sketching...
-
-
-class BeamHook(Enum):
-    RIGHT = auto()
-    LEFT = auto()
 
 
 @dataclass
 class BeamHint:
     break_depth: Optional[int] = None
-    hook: Optional[BeamHook] = None
+    hook: Optional[HorizontalDirection] = None
 
 
 @dataclass
 class BeamState:
-    count: int
+    flag_count: int
     break_depth: Optional[int] = None
-    hook: Optional[BeamHook] = None
+    hook: Optional[HorizontalDirection] = None
 
 
 # notice how BeamSpec is identical to BeamState...
@@ -43,7 +39,7 @@ class BeamSpec(NamedTuple):
     subdivision breaks unless explicitly requested with this field.
 
     """
-    hook: Optional[BeamHook] = None
+    hook: Optional[HorizontalDirection] = None
     """User-provided override for hook direction.
 
     This only has an effect if the beam position requires a hook and
@@ -72,22 +68,77 @@ def resolve_beams(specs: list[BeamSpec]) -> list[BeamState]:
             # First item in group or subgroup
             next_count = cast(int, next_count)
             if current_count > next_count:
-                hook = BeamHook.RIGHT
+                hook = HorizontalDirection.RIGHT
         elif next_count is None:
             # Last item in group
             prev_count = cast(int, prev_count)
             if current_count > prev_count:
-                hook = BeamHook.LEFT
+                hook = HorizontalDirection.LEFT
         else:
             # Item in middle of group
             if current_count > prev_count and current_count > next_count:
                 if prev_count < next_count:
-                    hook = BeamHook.RIGHT
+                    hook = HorizontalDirection.RIGHT
                 elif prev_count > next_count:
-                    hook = BeamHook.LEFT
+                    hook = HorizontalDirection.LEFT
                 else:
                     # Surrounding positions flag counts are equal, so
                     # hook direction is ambiguous. Allow override.
-                    hook = current_hint_hook or BeamHook.LEFT
+                    hook = current_hint_hook or HorizontalDirection.LEFT
         states.append(BeamState(current_count, break_depth, hook))
     return states
+
+
+class BeamPathSpec(NamedTuple):
+    depth: int
+    """The beam's 1-indexed vertical position in the beam stack.
+    
+    1 represents the outermost beam, 2 represents 1 inward, and so
+    on. For example, in a beam stack connecting sixteenth notes,
+    `depth=1` would represent the 8th note beam while `depth=2` would
+    represent the 16th note beam. In this way, `depth` corresponds to
+    the flag counts of represented durations.
+    """
+
+    start: int
+    """Index of the starting position"""
+
+    end: int | HorizontalDirection
+    """Index of the ending position or a hook direction.
+
+    If this is an index, it should be greater than `start`.
+    """
+
+
+def resolve_beam_layout(states: list[BeamState]) -> list[BeamPathSpec]:
+    """Given a list of individual note beam states, work out how to render the beams."""
+    # Iterate through beams from depth 0 to end, left to right.
+    path_specs = []
+    max_flag_count = max((state.flag_count for state in states))
+    for depth in range(1, max_flag_count + 1):
+        start_idx = None
+        for i, state in enumerate(states):
+            if start_idx is None:
+                if state.flag_count >= depth:
+                    start_idx = i
+                else:
+                    continue
+            if state.break_depth is not None and state.break_depth < depth:
+                break_ends_beam = True
+            else:
+                break_ends_beam = False
+            if i == len(states) - 1 or states[i + 1].flag_count < depth:
+                next_depth_ends_beam = True
+            else:
+                next_depth_ends_beam = False
+            if break_ends_beam or next_depth_ends_beam:
+                # Beam ends after this state
+                if start_idx == i:
+                    # Beam only spanned one state, treat it as a hook
+                    # Sanity check, hook should always be provided when required.
+                    assert state.hook
+                    path_specs.append(BeamPathSpec(depth, start_idx, state.hook))
+                else:
+                    path_specs.append(BeamPathSpec(depth, start_idx, i))
+                start_idx = None
+    return path_specs
--- tests/test_western/test_beam_group.py
+++ tests/test_western/test_beam_group.py
@@ -1,45 +1,52 @@
-from neoscore.western.beam_group import BeamHook, BeamSpec, BeamState, resolve_beams
+from neoscore.models.directions import HorizontalDirection
+from neoscore.western.beam_group import (
+    BeamPathSpec,
+    BeamSpec,
+    BeamState,
+    resolve_beam_layout,
+    resolve_beams,
+)
 
 
 def test_resolve_beams():
     assert resolve_beams([BeamSpec(3), BeamSpec(2), BeamSpec(1)]) == [
-        BeamState(3, hook=BeamHook.RIGHT),
+        BeamState(3, hook=HorizontalDirection.RIGHT),
         BeamState(2),
         BeamState(1),
     ]
 
     assert resolve_beams([BeamSpec(1), BeamSpec(3), BeamSpec(2)]) == [
         BeamState(1),
-        BeamState(3, hook=BeamHook.RIGHT),
+        BeamState(3, hook=HorizontalDirection.RIGHT),
         BeamState(2),
     ]
 
     assert resolve_beams([BeamSpec(1), BeamSpec(2), BeamSpec(1)]) == [
         BeamState(1),
-        BeamState(2, hook=BeamHook.LEFT),
+        BeamState(2, hook=HorizontalDirection.LEFT),
         BeamState(1),
     ]
 
     assert resolve_beams([BeamSpec(2), BeamSpec(3), BeamSpec(1)]) == [
         BeamState(2),
-        BeamState(3, hook=BeamHook.LEFT),
+        BeamState(3, hook=HorizontalDirection.LEFT),
         BeamState(1),
     ]
 
     assert resolve_beams([BeamSpec(3), BeamSpec(2), BeamSpec(3)]) == [
-        BeamState(3, hook=BeamHook.RIGHT),
+        BeamState(3, hook=HorizontalDirection.RIGHT),
         BeamState(2),
-        BeamState(3, hook=BeamHook.LEFT),
+        BeamState(3, hook=HorizontalDirection.LEFT),
     ]
 
     assert resolve_beams([BeamSpec(1), BeamSpec(2), BeamSpec(3)]) == [
         BeamState(1),
         BeamState(2),
-        BeamState(3, hook=BeamHook.LEFT),
+        BeamState(3, hook=HorizontalDirection.LEFT),
     ]
 
     assert resolve_beams([BeamSpec(3), BeamSpec(2), BeamSpec(2)]) == [
-        BeamState(3, hook=BeamHook.RIGHT),
+        BeamState(3, hook=HorizontalDirection.RIGHT),
         BeamState(2),
         BeamState(2),
     ]
@@ -47,7 +54,7 @@ def test_resolve_beams():
 
 def test_resolve_beams_with_hook_hints():
     assert resolve_beams(
-        [BeamSpec(2), BeamSpec(2, hook=BeamHook.LEFT), BeamSpec(1)]
+        [BeamSpec(2), BeamSpec(2, hook=HorizontalDirection.LEFT), BeamSpec(1)]
     ) == [
         BeamState(2),  # Invalid hook hint is ignored
         BeamState(2),
@@ -55,7 +62,7 @@ def test_resolve_beams_with_hook_hints():
     ]
 
     assert resolve_beams(
-        [BeamSpec(2, hook=BeamHook.RIGHT), BeamSpec(2), BeamSpec(1)]
+        [BeamSpec(2, hook=HorizontalDirection.RIGHT), BeamSpec(2), BeamSpec(1)]
     ) == [
         BeamState(2),
         BeamState(2),  # Invalid hook hint is ignored
@@ -63,10 +70,10 @@ def test_resolve_beams_with_hook_hints():
     ]
 
     assert resolve_beams(
-        [BeamSpec(1), BeamSpec(2, hook=BeamHook.RIGHT), BeamSpec(1)]
+        [BeamSpec(1), BeamSpec(2, hook=HorizontalDirection.RIGHT), BeamSpec(1)]
     ) == [
         BeamState(1),
-        BeamState(2, hook=BeamHook.RIGHT),
+        BeamState(2, hook=HorizontalDirection.RIGHT),
         BeamState(1),
     ]
 
@@ -93,6 +100,67 @@ def test_resolve_beams_with_break_depths_affecting_flags():
     assert resolve_beams([BeamSpec(2), BeamSpec(2, 1), BeamSpec(3), BeamSpec(2)]) == [
         BeamState(2),
         BeamState(2, 1),
-        BeamState(3, None, BeamHook.RIGHT),
+        BeamState(3, None, HorizontalDirection.RIGHT),
         BeamState(2),
+    ]
+
+
+def test_resolve_beam_layout():
+    # Simplest case of 2 8th notes
+    assert resolve_beam_layout([BeamState(1), BeamState(1)]) == [
+        BeamPathSpec(1, 0, 1),
+    ]
+
+    # 2 16th notes
+    assert resolve_beam_layout([BeamState(2), BeamState(2)]) == [
+        BeamPathSpec(1, 0, 1),
+        BeamPathSpec(2, 0, 1),
+    ]
+
+    # Several 64th notes
+    assert resolve_beam_layout([BeamState(4)] * 10) == [
+        BeamPathSpec(1, 0, 9),
+        BeamPathSpec(2, 0, 9),
+        BeamPathSpec(3, 0, 9),
+        BeamPathSpec(4, 0, 9),
+    ]
+
+    # 32nd notes with 2 groups of 16ths
+    assert resolve_beam_layout(
+        [BeamState(3), BeamState(3, 1), BeamState(3), BeamState(3)]
+    ) == [
+        BeamPathSpec(1, 0, 3),
+        BeamPathSpec(2, 0, 1),
+        BeamPathSpec(2, 2, 3),
+        BeamPathSpec(3, 0, 1),
+        BeamPathSpec(3, 2, 3),
+    ]
+
+    # Starting and ending hooks
+    assert resolve_beam_layout(
+        [
+            BeamState(3, hook=HorizontalDirection.RIGHT),
+            BeamState(2),
+            BeamState(3, hook=HorizontalDirection.LEFT),
+        ]
+    ) == [
+        BeamPathSpec(1, 0, 2),
+        BeamPathSpec(2, 0, 2),
+        BeamPathSpec(3, 0, HorizontalDirection.RIGHT),
+        BeamPathSpec(3, 2, HorizontalDirection.LEFT),
+    ]
+
+    # Subgroups and a hook
+    assert resolve_beam_layout(
+        [
+            BeamState(2),
+            BeamState(2, 1),
+            BeamState(3, None, HorizontalDirection.RIGHT),
+            BeamState(2),
+        ]
+    ) == [
+        BeamPathSpec(1, 0, 3),
+        BeamPathSpec(2, 0, 1),
+        BeamPathSpec(2, 2, 3),
+        BeamPathSpec(3, 2, HorizontalDirection.RIGHT),
     ]
