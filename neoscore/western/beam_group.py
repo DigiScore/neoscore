eb3cba046593546a70b86fc74fb1d8af94dbf16b
Andrew Yoon <andrew@nothing-to-say.org>
2022-03-29 22:08:34 -0400

    More work on beam layout

--- neoscore/western/beam_group.py
+++ neoscore/western/beam_group.py
@@ -1,54 +1,93 @@
 from dataclasses import dataclass
 from enum import Enum, auto
-from typing import Optional, cast
-
-from neoscore.models.duration_display import DurationDisplay
+from typing import NamedTuple, Optional, cast
 
 # wip sketching...
 
 
-# @dataclass
-# class BeamGroup:
-
-
-# I think this algorithm actually doesn't even need to know the real
-# duration values - it just needs to know flag counts.
-
-
 class BeamHook(Enum):
-    NONE = auto()
     RIGHT = auto()
     LEFT = auto()
 
 
 @dataclass
+class BeamHint:
+    break_depth: Optional[int] = None
+    hook: Optional[BeamHook] = None
+
+
+@dataclass
 class BeamState:
     count: int
-    hook: BeamHook = BeamHook.NONE
     break_depth: Optional[int] = None
+    hook: Optional[BeamHook] = None
 
 
-def resolve_beams(durs: list[DurationDisplay]) -> list[BeamState]:
-    if len(durs) < 2:
+# notice how BeamSpec is identical to BeamState...
+
+
+class BeamSpec(NamedTuple):
+    flag_count: int
+    break_depth: Optional[int] = None
+    """User-provided override setting break depths.
+
+    This indicates the number of beams to cut a subdivision to after
+    this beam position. The value must be less than `flag_count` and
+    greater than 0.
+
+    For example, `[BeamSpec(3), BeamSpec(3, break_depth=1),
+    BeamSpec(3), BeamSpec(3)]` encodes four 32nd notes subdivided into
+    2 groups of 2 connected by a single beam.
+    
+    Because the beam resolver is not meter-aware, it does not perform
+    subdivision breaks unless explicitly requested with this field.
+
+    """
+    hook: Optional[BeamHook] = None
+    """User-provided override for hook direction.
+
+    This only has an effect if the beam position requires a hook and
+    that hook direction is ambiguous. This only applies in places
+    where, within a beam subgroup, a position has more flags than its
+    previous and following position, *and* those adjacent values are
+    equal. For example, a sixteenth note surrounded by two eighth
+    notes.
+    """
+
+
+def resolve_beams(specs: list[BeamSpec]) -> list[BeamState]:
+    if len(specs) < 2:
         raise ValueError("Beam groups must have at least 2 members")
     states: list[BeamState] = []
-    beam_counts = [dur.flag_count for dur in durs]  # assume all durs have flags
-    for i, current_count in enumerate(beam_counts):
-        prev_count = beam_counts[i - 1] if i > 0 else None
-        next_count = beam_counts[i + 1] if i < len(beam_counts) - 1 else None
-        hook = BeamHook.NONE
-        if prev_count is None:
-            # First item in group
+    for i, (current_count, break_depth, current_hint_hook) in enumerate(specs):
+        prev_count, prev_hint_break_depth, _ = (
+            specs[i - 1] if i > 0 else (None, None, None)
+        )
+        next_count, next_hint_break_depth, _ = (
+            specs[i + 1] if i < len(specs) - 1 else (None, None, None)
+        )
+        # Resolve hook
+        hook = None
+        if prev_count is None or (prev_hint_break_depth and next_count):
+            # First item in group or subgroup
             next_count = cast(int, next_count)
-            if next_count < current_count:
+            if current_count > next_count:
                 hook = BeamHook.RIGHT
         elif next_count is None:
             # Last item in group
-            if prev_count < current_count:
+            prev_count = cast(int, prev_count)
+            if current_count > prev_count:
                 hook = BeamHook.LEFT
         else:
             # Item in middle of group
             if current_count > prev_count and current_count > next_count:
-                hook = BeamHook.LEFT
-        states.append(BeamState(current_count, hook))
+                if prev_count < next_count:
+                    hook = BeamHook.RIGHT
+                elif prev_count > next_count:
+                    hook = BeamHook.LEFT
+                else:
+                    # Surrounding positions flag counts are equal, so
+                    # hook direction is ambiguous. Allow override.
+                    hook = current_hint_hook or BeamHook.LEFT
+        states.append(BeamState(current_count, break_depth, hook))
     return states
--- tests/test_western/test_beam_group.py
+++ tests/test_western/test_beam_group.py
@@ -1,38 +1,98 @@
-from neoscore.models.duration_display import DurationDisplay
-from neoscore.western.beam_group import BeamHook, BeamState, resolve_beams
-
-
-def make_durs(flag_counts: list[int]):
-    return [DurationDisplay(2 ** (count + 2), 0) for count in flag_counts]
+from neoscore.western.beam_group import BeamHook, BeamSpec, BeamState, resolve_beams
 
 
 def test_resolve_beams():
-    assert resolve_beams(make_durs([3, 2, 1])) == [
-        BeamState(3, BeamHook.RIGHT),
+    assert resolve_beams([BeamSpec(3), BeamSpec(2), BeamSpec(1)]) == [
+        BeamState(3, hook=BeamHook.RIGHT),
         BeamState(2),
         BeamState(1),
     ]
 
-    assert resolve_beams(make_durs([2, 3, 1])) == [
+    assert resolve_beams([BeamSpec(1), BeamSpec(3), BeamSpec(2)]) == [
+        BeamState(1),
+        BeamState(3, hook=BeamHook.RIGHT),
         BeamState(2),
-        BeamState(3, BeamHook.LEFT),
+    ]
+
+    assert resolve_beams([BeamSpec(1), BeamSpec(2), BeamSpec(1)]) == [
+        BeamState(1),
+        BeamState(2, hook=BeamHook.LEFT),
         BeamState(1),
     ]
 
-    assert resolve_beams(make_durs([3, 2, 3])) == [
-        BeamState(3, BeamHook.RIGHT),
+    assert resolve_beams([BeamSpec(2), BeamSpec(3), BeamSpec(1)]) == [
         BeamState(2),
-        BeamState(3, BeamHook.LEFT),
+        BeamState(3, hook=BeamHook.LEFT),
+        BeamState(1),
     ]
 
-    assert resolve_beams(make_durs([1, 2, 3])) == [
+    assert resolve_beams([BeamSpec(3), BeamSpec(2), BeamSpec(3)]) == [
+        BeamState(3, hook=BeamHook.RIGHT),
+        BeamState(2),
+        BeamState(3, hook=BeamHook.LEFT),
+    ]
+
+    assert resolve_beams([BeamSpec(1), BeamSpec(2), BeamSpec(3)]) == [
         BeamState(1),
         BeamState(2),
-        BeamState(3, BeamHook.LEFT),
+        BeamState(3, hook=BeamHook.LEFT),
     ]
 
-    assert resolve_beams(make_durs([3, 2, 2])) == [
-        BeamState(3, BeamHook.RIGHT),
+    assert resolve_beams([BeamSpec(3), BeamSpec(2), BeamSpec(2)]) == [
+        BeamState(3, hook=BeamHook.RIGHT),
         BeamState(2),
+        BeamState(2),
+    ]
+
+
+def test_resolve_beams_with_hook_hints():
+    assert resolve_beams(
+        [BeamSpec(2), BeamSpec(2, hook=BeamHook.LEFT), BeamSpec(1)]
+    ) == [
+        BeamState(2),  # Invalid hook hint is ignored
+        BeamState(2),
+        BeamState(1),
+    ]
+
+    assert resolve_beams(
+        [BeamSpec(2, hook=BeamHook.RIGHT), BeamSpec(2), BeamSpec(1)]
+    ) == [
+        BeamState(2),
+        BeamState(2),  # Invalid hook hint is ignored
+        BeamState(1),
+    ]
+
+    assert resolve_beams(
+        [BeamSpec(1), BeamSpec(2, hook=BeamHook.RIGHT), BeamSpec(1)]
+    ) == [
+        BeamState(1),
+        BeamState(2, hook=BeamHook.RIGHT),
+        BeamState(1),
+    ]
+
+
+def test_resolve_beams_break_depths_copied():
+    assert resolve_beams([BeamSpec(3), BeamSpec(3, 1), BeamSpec(3)]) == [
+        BeamState(3),
+        BeamState(3, 1),
+        BeamState(3),
+    ]
+    assert resolve_beams([BeamSpec(3, 2), BeamSpec(3, 1), BeamSpec(3)]) == [
+        BeamState(3, 2),
+        BeamState(3, 1),
+        BeamState(3),
+    ]
+    assert resolve_beams([BeamSpec(3), BeamSpec(3), BeamSpec(3, 1)]) == [
+        BeamState(3),
+        BeamState(3),
+        BeamState(3, 1),
+    ]
+
+
+def test_resolve_beams_with_break_depths_affecting_flags():
+    assert resolve_beams([BeamSpec(2), BeamSpec(2, 1), BeamSpec(3), BeamSpec(2)]) == [
+        BeamState(2),
+        BeamState(2, 1),
+        BeamState(3, None, BeamHook.RIGHT),
         BeamState(2),
     ]
