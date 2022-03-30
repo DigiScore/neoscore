5c9b3502324034ee7a4957a0f3519f30d9cce611
craigv <vear2ev2.co.uk>
2022-03-30 10:55:10 +0100

    updated MusicChar.py to use property access not dict lookup

--- neoscore/core/music_char.py
+++ neoscore/core/music_char.py
@@ -27,7 +27,7 @@ class MusicChar:
     ######## PUBLIC PROPERTIES ########
 
     @cached_property
-    def glyph_info(self) -> dict:
+    def glyph_info(self) -> object:
         # this is a little expensive, so only do it on demand and then cache it
         return self.font.glyph_info(self.glyph_name, self.alternate_number)
 
@@ -37,7 +37,7 @@ class MusicChar:
 
         This is a convenience property for `glyph_info["canonicalName"]`
         """
-        return self.glyph_info["canonicalName"]
+        return self.glyph_info.canonical_name
 
     @property
     def codepoint(self):
@@ -45,14 +45,48 @@ class MusicChar:
 
         This is a convenience property for `glyph_info["codepoint"]`
         """
-        return self.glyph_info["codepoint"]
+        return self.glyph_info.codepoint
 
     @property
     def bounding_rect(self):
         """Rect: The glyph bounding box."""
         # I think y and h here are wrong...
-        x = self.glyph_info["glyphBBox"]["bBoxSW"][0]
-        y = self.glyph_info["glyphBBox"]["bBoxNE"][1]
-        w = self.glyph_info["glyphBBox"]["bBoxNE"][0] - x
-        h = (self.glyph_info["glyphBBox"]["bBoxSW"][1] - y) * -1
+        x = self.glyph_info.glyphBBox["bBoxSW"][0]
+        y = self.glyph_info.glyphBBox["bBoxNE"][1]
+        w = self.glyph_info.glyphBBox["bBoxNE"][0] - x
+        h = (self.glyph_info.glyphBBox["bBoxSW"][1] - y) * -1
         return Rect(x, y, w, h)
+
+
+    #
+    #
+    # @cached_property
+    # def glyph_info(self) -> dict:
+    #     # this is a little expensive, so only do it on demand and then cache it
+    #     return self.font.glyph_info(self.glyph_name, self.alternate_number)
+    #
+    # @property
+    # def canonical_name(self):
+    #     """The canonical SMuFL glyph name.
+    #
+    #     This is a convenience property for `glyph_info["canonicalName"]`
+    #     """
+    #     return self.glyph_info["canonicalName"]
+    #
+    # @property
+    # def codepoint(self):
+    #     """The glyph's SMuFL codepoint.
+    #
+    #     This is a convenience property for `glyph_info["codepoint"]`
+    #     """
+    #     return self.glyph_info["codepoint"]
+    #
+    # @property
+    # def bounding_rect(self):
+    #     """Rect: The glyph bounding box."""
+    #     # I think y and h here are wrong...
+    #     x = self.glyph_info["glyphBBox"]["bBoxSW"][0]
+    #     y = self.glyph_info["glyphBBox"]["bBoxNE"][1]
+    #     w = self.glyph_info["glyphBBox"]["bBoxNE"][0] - x
+    #     h = (self.glyph_info["glyphBBox"]["bBoxSW"][1] - y) * -1
+    #     return Rect(x, y, w, h)
