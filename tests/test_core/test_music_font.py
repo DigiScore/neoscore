1af2098055d749f7be53f298d42be09e2329d677
craigv <vear2ev2.co.uk>
2022-04-01 18:01:32 +0100

    debugging and testing

--- tests/test_core/test_music_font.py
+++ tests/test_core/test_music_font.py
@@ -1,5 +1,11 @@
+import unittest
+import pytest
+from neoscore.core import neoscore
 from neoscore.core.music_font import MusicFont
 from neoscore.utils.units import Mm, Unit
+from neoscore.utils import smufl
+from neoscore.models.glyph_info import GlyphInfo
+from neoscore.utils.exceptions import MusicFontGlyphNotFoundError
 
 from ..helpers import AppTest
 
@@ -36,3 +42,34 @@ class TestMusicFont(AppTest):
         # (Can't test case of different family name since only Bravura exists)
         # assert hash(font) != MusicFont("Foo", Unit)
         assert hash(font) != hash(MusicFont("Bravura", Mm))
+
+    def test_glyph_info_for_all_required_glyphs(self):
+        font = MusicFont("Bravura", Unit)
+        # test each glyph in the glyphnamelist json
+        for testGlyph in smufl.glyph_names:
+            test = font.glyph_info(testGlyph).canonical_name
+            raw = font._glyph_info(testGlyph).canonical_name
+            # assert test == testGlyph
+            assert test == raw
+
+    def test_glyph_info_for_one_alternate_glyph(self):
+        font = MusicFont("Bravura", Unit)
+        testGlyph = font.glyph_info('brace', 1)
+        assert testGlyph.canonical_name == 'braceSmall'
+        assert testGlyph.codepoint == "\uF400"
+
+    def test_glyph_info_for_last_alternate_glyph(self):
+        font = MusicFont("Bravura", Unit)
+        testGlyph = font.glyph_info('brace', 4)
+        assert testGlyph.canonical_name == 'braceFlat'
+        assert testGlyph.codepoint == "\uF403"
+
+    def test_glyph_info_for_Foo_glyph(self):
+        font = MusicFont("Bravura", Unit)
+        with pytest.raises(MusicFontGlyphNotFoundError):
+            font.glyph_info('Foo')
+
+    def test_glyph_info_for_out_of_range_alternative_glyph(self):
+        font = MusicFont("Bravura", Unit)
+        with pytest.raises(MusicFontGlyphNotFoundError):
+            font.glyph_info('brace', 6)
