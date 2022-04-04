fbe278a4b75710d58473724efe319891057561cb
Andrew Yoon <andrew@nothing-to-say.org>
2022-04-04 16:44:41 -0400

    Try again to fix unimport CI check

--- dev_scripts/pre-commit.sh
+++ dev_scripts/pre-commit.sh
@@ -8,7 +8,7 @@ echo "Removing unused imports"
 # Note that this returns exit code 1 when imports are fixed
 # so need to disable error bailing here
 set +e
-unimport --check --remove --exclude neoscore/common.py
+unimport --check --remove --exclude neoscore/common.py|.venv
 set -e
 echo "Sorting imports"
 isort .
