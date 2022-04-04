3033caafd634c1d6bc7ff5aee7b6358f22c1716a
Craig Vear <craigvear123@gmail.com>
2022-04-04 17:29:32 +0100

    music_font new _glyph_info (#5)
    Fixes #2 completed. tested. cleanup script applied

--- neoscore/core/music_char.py
+++ neoscore/core/music_char.py
@@ -3,7 +3,7 @@ from functools import cached_property
 from typing import Optional
 
 from neoscore.core.music_font import MusicFont
-from neoscore.utils.rect import Rect
+from neoscore.models.glyph_info import GlyphInfo
 
 
 @dataclass(frozen=True)
@@ -27,32 +27,29 @@ class MusicChar:
     ######## PUBLIC PROPERTIES ########
 
     @cached_property
-    def glyph_info(self) -> dict:
+    def glyph_info(self) -> GlyphInfo:
         # this is a little expensive, so only do it on demand and then cache it
+        """The metadata for a specified glyphname"""
+
         return self.font.glyph_info(self.glyph_name, self.alternate_number)
 
     @property
     def canonical_name(self):
         """The canonical SMuFL glyph name.
 
-        This is a convenience property for `glyph_info["canonicalName"]`
+        This is a convenience property for `glyph_info.canonical_name`
         """
-        return self.glyph_info["canonicalName"]
+        return self.glyph_info.canonical_name
 
     @property
     def codepoint(self):
         """The glyph's SMuFL codepoint.
 
-        This is a convenience property for `glyph_info["codepoint"]`
+        This is a convenience property for glyph_info.codepoint`
         """
-        return self.glyph_info["codepoint"]
+        return self.glyph_info.codepoint
 
     @property
     def bounding_rect(self):
         """Rect: The glyph bounding box."""
-        # I think y and h here are wrong...
-        x = self.glyph_info["glyphBBox"]["bBoxSW"][0]
-        y = self.glyph_info["glyphBBox"]["bBoxNE"][1]
-        w = self.glyph_info["glyphBBox"]["bBoxNE"][0] - x
-        h = (self.glyph_info["glyphBBox"]["bBoxSW"][1] - y) * -1
-        return Rect(x, y, w, h)
+        return self.glyph_info.bounding_rect
