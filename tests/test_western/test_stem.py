65f7a1ffc072d524d23dcbb5a85d481a6c0b3636
craigv <vear2ev2.co.uk>
2022-04-11 10:50:28 +0100

    added extra testing for stem and line directions. all working. pre-commit done.

--- tests/test_western/test_stem.py
+++ tests/test_western/test_stem.py
@@ -1,14 +1,12 @@
 from neoscore.core.directions import VerticalDirection
 from neoscore.core.flowable import Flowable
+from neoscore.core.path_element import LineTo, MoveTo
+from neoscore.core.point import Point
 from neoscore.core.units import Mm
-from neoscore.western.chordrest import Chordrest
-from neoscore.western.clef import Clef
-from neoscore.western.duration import Duration
-from neoscore.western.octave_line import OctaveLine
 from neoscore.western.staff import Staff
 from neoscore.western.stem import Stem
 
-from ..helpers import AppTest
+from ..helpers import AppTest, assert_path_els_equal
 
 
 class TestStem(AppTest):
@@ -26,5 +24,32 @@ class TestStem(AppTest):
         assert stem.direction == VerticalDirection.DOWN
         assert stem.direction.value == 1
 
+    def test_default_pen_uses_engraving_default_thickness(self):
+        stem = Stem((Mm(0), Mm(0)), self.staff, VerticalDirection.UP, Mm(10))
+        assert (
+            stem.pen.thickness
+            == self.staff.music_font.engraving_defaults["stemThickness"]
+        )
+
     def test_stem_line_respects_stem_direction(self):
-        pass
+        # stem direction UP
+        stem = Stem((Mm(0), Mm(0)), self.staff, VerticalDirection.UP, Mm(10))
+        assert stem.pos == Point(Mm(0), Mm(0))
+        assert len(stem.elements) == 2
+
+        # path origin moves to x, y == 0,0
+        assert_path_els_equal(stem.elements[0], MoveTo(Point(Mm(0), Mm(0)), stem))
+
+        # path draw line -10 (up direction)
+        assert_path_els_equal(stem.elements[1], LineTo(Point(Mm(0), Mm(-10)), stem))
+
+        # stem direction DOWN
+        stem = Stem((Mm(10), Mm(10)), self.staff, VerticalDirection.DOWN, Mm(10))
+        assert stem.pos == Point(Mm(10), Mm(10))
+        assert len(stem.elements) == 2
+
+        # path origin moves to x, y == 0,0
+        assert_path_els_equal(stem.elements[0], MoveTo(Point(Mm(0), Mm(0)), stem))
+
+        # path draw line +10 (down direction)
+        assert_path_els_equal(stem.elements[1], LineTo(Point(Mm(0), Mm(10)), stem))
