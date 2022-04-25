524d34a7941a53069dbdd0931289adef8c6a56be
craigv <vear2ev2.co.uk>
2022-04-25 21:43:13 +0100

    unconnected working. vtest complete. tests to do

--- neoscore/western/barline.py
+++ neoscore/western/barline.py
@@ -35,6 +35,7 @@ class Barline(PositionedObject, MultiStaffObject, HasMusicFont):
         staves: list[StaffLike],
         styles: BarlineStyle | Iterable[BarlineStyle] = barline_style.SINGLE,
         font: Optional[MusicFont] = None,
+        connected: Optional[bool] = True
     ):
         """
         Args:
@@ -53,6 +54,7 @@ class Barline(PositionedObject, MultiStaffObject, HasMusicFont):
         self.engraving_defaults = self._music_font.engraving_defaults
         self.paths = []
         self.staves = staves
+        self.connected = connected
 
         # Start x position for first barline relative to self
         start_x = ZERO
@@ -74,7 +76,7 @@ class Barline(PositionedObject, MultiStaffObject, HasMusicFont):
 
     #### PRIVATE METHODS ####
     def _draw_barline(
-        self, start_x: Unit, thickness: Unit, pen_pattern: PenPattern, color: ColorDef
+            self, start_x: Unit, thickness: Unit, pen_pattern: PenPattern, color: ColorDef
     ):
         # Create the path
         line_path = Path(
@@ -85,10 +87,19 @@ class Barline(PositionedObject, MultiStaffObject, HasMusicFont):
 
         # Draw the path
         # move to counter the barline extant offset
-        line_path.move_to(ZERO, self.highest.barline_extent[0])
-        line_path.line_to(
-            ZERO, map_between(self, self.lowest).y + self.lowest.barline_extent[1]
-        )
+        if self.connected:
+            line_path.move_to(ZERO, self.highest.barline_extent[0])
+            line_path.line_to(
+                ZERO, map_between(self, self.lowest).y + self.lowest.barline_extent[1]
+            )
+        else:
+            y_offset = self.highest.y
+            for stave in self.staves:
+                new_y = stave.pos.y - y_offset
+                line_path.move_to(ZERO, new_y + stave.barline_extent[0])
+                line_path.line_to(
+                    ZERO, new_y + stave.barline_extent[1]
+                )
 
         self.paths.append(line_path)
 
