740f74d44f11ab57eda1c0bf7b84bcb4dad5e743
Andrew Yoon <andrew@nothing-to-say.org>
2022-11-19 10:19:30 -0500

    run precommit scripts

--- tests/test_western/test_pedal_line.py
+++ tests/test_western/test_pedal_line.py
@@ -1,10 +1,12 @@
-from ..helpers import AppTest
 import pytest
 
-from neoscore.western.staff import Staff
-from neoscore.western.pedal_line import PedalLine
+from neoscore.core.point import ORIGIN, Point
 from neoscore.core.units import Mm
-from neoscore.core.point import Point, ORIGIN
+from neoscore.western.pedal_line import PedalLine
+from neoscore.western.staff import Staff
+
+from ..helpers import AppTest
+
 
 class TestPedalLine(AppTest):
     def setUp(self):
@@ -12,28 +14,25 @@ class TestPedalLine(AppTest):
         self.staff = Staff(ORIGIN, None, Mm(100))
 
     def test_full_pedal_line(self):
-        pedal_line = PedalLine(ORIGIN,
-                               self.staff,
-                               Mm(100))
+        pedal_line = PedalLine(ORIGIN, self.staff, Mm(100))
         assert pedal_line.pos == Point(x=Mm(0.0), y=Mm(0.0))
         assert pedal_line.end_pos == Point(x=Mm(100.0), y=Mm(0.0))
         assert pedal_line.elements[0].pos == Point(x=Mm(0.0), y=Mm(0.0))
         assert pedal_line.elements[3].pos == Point(x=Mm(100.0), y=Mm(0.0))
 
     def test_pedal_line_half_lift(self):
-        pedal_line = PedalLine(ORIGIN,
-                               self.staff,
-                               Mm(100),
-                               half_lift_positions=[Mm(10), Mm(50), Mm(95)]
-                               )
+        pedal_line = PedalLine(
+            ORIGIN, self.staff, Mm(100), half_lift_positions=[Mm(10), Mm(50), Mm(95)]
+        )
         assert pedal_line.elements[3].pos == Point(x=Mm(10.0), y=Mm(0.0))
         assert pedal_line.elements[6].pos == Point(x=Mm(50.0), y=Mm(0.0))
         assert pedal_line.elements[9].pos == Point(x=Mm(95.0), y=Mm(0.0))
 
     def test_pedal_line_half_lift_too_long(self):
         with pytest.raises(AttributeError):
-            PedalLine(ORIGIN,
-                      self.staff,
-                      Mm(100),
-                      half_lift_positions=[Mm(10), Mm(50), Mm(120)]
-                      )
+            PedalLine(
+                ORIGIN,
+                self.staff,
+                Mm(100),
+                half_lift_positions=[Mm(10), Mm(50), Mm(120)],
+            )
