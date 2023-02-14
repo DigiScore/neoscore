664a6083606a6bf75be100ba60c9be190200065a
Andrew Yoon <andrew@nothing-to-say.org>
2023-02-13 23:51:24 -0500

    Improve demo glyphs in `other_music_fonts` example

--- examples/other_music_fonts.py
+++ examples/other_music_fonts.py
@@ -1,7 +1,7 @@
 from helpers import render_example
 
 from neoscore.core import neoscore
-from neoscore.core.repeating_music_text_line import RepeatingMusicTextLine
+from neoscore.core.music_text import MusicText
 from neoscore.core.units import ZERO, Mm
 from neoscore.western.chordrest import Chordrest
 from neoscore.western.clef import Clef
@@ -15,26 +15,19 @@ neoscore.setup()
 # https://github.com/fkretlow/sebastian
 #
 # e.g. /path/to/Sebastian.otf
-FONT_FILE_PATH = ""
+FONT_FILE_PATH = "/home/ayoon/Downloads/Sebastian.fonts.v1.08/Sebastian.otf"
 # e.g. /path/to/Sebastian.json
-METADATA_PATH = ""
+METADATA_PATH = "/home/ayoon/Downloads/Sebastian.fonts.v1.08/Sebastian.json"
 # e.g. "Sebastian"
-FONT_FAMILY_NAME = ""
+FONT_FAMILY_NAME = "Sebastian"
 
 neoscore.register_music_font(FONT_FILE_PATH, METADATA_PATH)
 
-staff = Staff((Mm(40), Mm(30)), None, Mm(100), music_font_family="Sebastian")
+staff = Staff((Mm(40), Mm(30)), None, Mm(100), music_font_family=FONT_FAMILY_NAME)
 Clef(ZERO, staff, "treble")
 KeySignature(ZERO, staff, "b_major")
 TimeSignature(ZERO, staff, (10, 16))
 Chordrest(ZERO, staff, ["f", "a", "b", "dn"], (3, 32))
-RepeatingMusicTextLine(
-    (ZERO, Mm(-3)),
-    staff,
-    (Mm(15), ZERO),
-    None,
-    "ornamentZigZagLineWithRightEnd",
-    "ornamentTrill",
-)
+MusicText((ZERO, Mm(-3)), staff, ["ornamentTrill", "ornamentTurn", "ornamentMordent"])
 
 render_example("other_music_fonts")
