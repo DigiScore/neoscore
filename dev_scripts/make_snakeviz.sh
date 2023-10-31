1c2d3b9f7bc9aa2c62217b0a9d3c7a32e9ee18a9
Demi Oshin <demithetechie@gmail.com>
2023-10-31 12:33:51 +0000

    change to python3 for snakeviz

--- dev_scripts/make_snakeviz.sh
+++ dev_scripts/make_snakeviz.sh
@@ -2,6 +2,6 @@
 
 set -o xtrace
 mkdir tmp
-python -m cProfile -o tmp/out.prof examples/kitchen_sink.py --image
-snakeviz --server tmp/out.prof
+python3 -m cProfile -o tmp/out.prof examples/kitchen_sink.py --image
+python3 -m snakeviz --server tmp/out.prof
 rm -r tmp
