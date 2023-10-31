9fd134dcd414143ebf7996df5cbf1facc7596b91
Demi Oshin <demithetechie@gmail.com>
2023-10-31 12:31:10 +0000

    change to pip3

--- dev_scripts/install_devtools.sh
+++ dev_scripts/install_devtools.sh
@@ -1,4 +1,4 @@
 # Run from within venv
 # `poetry install`
 # `poetry shell` then..
-pip install 'python-lsp-server[all]' ipython snakeviz gprof2dot pyls-isort pylsp-mypy python-lsp-black pytest-watch
+pip3 install 'python-lsp-server[all]' ipython snakeviz gprof2dot pyls-isort pylsp-mypy python-lsp-black pytest-watch
