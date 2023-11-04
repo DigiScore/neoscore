5bf888941e274d9fe4c9f017f2ae811562fcba2f
Demi Oshin <demithetechie@gmail.com>
2023-11-04 08:32:17 +0000

    edit script to export csv for sink and circle

--- dev_scripts/benchmark.sh
+++ dev_scripts/benchmark.sh
@@ -1,3 +1,4 @@
 # Benchmark the example with https://github.com/sharkdp/hyperfine
 
-hyperfine --show-output --export-csv benchmark_results.csv 'python3 examples/kitchen_sink.py --image'
+hyperfine --show-output --export-csv benchmark_results_sink.csv 'python3 examples/kitchen_sink.py --image'
+hyperfine --show-output --export-csv benchmark_results_circle.csv 'python3 examples/circle.py --image'
